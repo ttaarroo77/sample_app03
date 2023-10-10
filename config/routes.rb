f7965f7c381d7441c05e0b898634b2919d20d06b
Tarou Nakazawa <144759562+ttaarroo77@users.noreply.github.com>
2023-10-10 06:33:14 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
+  #GET / => static_pages#home
+  root "static_pages#home"
+
+  # GET /static_pages/home => static_pages#home
+  # GET /about => static_pages#about
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/about"
 end
